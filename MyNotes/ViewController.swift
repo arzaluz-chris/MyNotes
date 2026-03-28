a548a80114f756c7d711b79387f03b508a7d4ffb
arzaluz-chris <23008381@es.uveg.edu.mx>
2026-03-28 12:45:04 -0600

    Initial Commit

---
+++ MyNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNotes
+//
+//  Created by Christian Arzaluz on 28/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
